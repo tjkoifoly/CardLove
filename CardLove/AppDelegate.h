1abdd69a86efe69e4363276de3d79793d87f81c7
tjkoifoly <foly.v1.8.01@gmail.com>
2013-02-23 10:06:01 +0700

    Initial Commit

---
+++ CardLove/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CardLove
+//
+//  Created by MAC on 2/23/13.
+//  Copyright (c) 2013 FOLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
